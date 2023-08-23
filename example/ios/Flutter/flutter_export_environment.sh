e0cf777e4ea8870596f7d150c33c87641cbf5987
willroymyles <willroymyles@yahoo.com>
2023-08-23 01:10:31 -0500

    updated ciiels and floors

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/wisdomekeh/dev/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/wisdomekeh/workspace/flutter/flutter_money_formatter/example"
+export "FLUTTER_ROOT=/Users/mmjaadmin/fvm/versions/2.12.0-4.1.pre"
+export "FLUTTER_APPLICATION_PATH=/Users/mmjaadmin/Projects/flutter_money_formatter/example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
 export "DART_OBFUSCATION=false"
-export "TRACK_WIDGET_CREATION=true"
+export "TRACK_WIDGET_CREATION=false"
 export "TREE_SHAKE_ICONS=false"
-export "PACKAGE_CONFIG=.dart_tool/package_config.json"
+export "PACKAGE_CONFIG=.packages"
--- lib/src/utils/compact_format_type.dart
+++ lib/src/utils/compact_format_type.dart
@@ -1,2 +1,2 @@
 /// An enum to be used on compact text format
-enum CompactFormatType { short, long }
+enum CompactFormatType { short, long, exact }
